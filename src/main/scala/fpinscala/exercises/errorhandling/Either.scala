509d8cc3a47ab38b944c700012a46e9aa1ccbdb1
Cheeshian Chuah <shian15810@gmail.com>
2024-12-24 08:59:51 +0000

    complete exercise 4.6 with passing tests

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -8,26 +8,38 @@ enum Either[+E,+A]:
   case Left(get: E)
   case Right(get: A)
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match
+    case Left(e) => b
+    case Right(a) => Right(a)
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  // def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+  //   this.flatMap(a => b.map(b => f(a, b)))
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for
+      a <- this
+      bb <- b
+    yield f(a, bb)
 
 object Either:
   def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
 
   def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
     if xs.isEmpty then
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Throwable, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Throwable, Int] =
     try Right(x / y)
     catch case NonFatal(t) => Left(t)
 
--- src/main/scala/fpinscala/worksheets/errorhandling/Either.worksheet.sc
+++ src/main/scala/fpinscala/worksheets/errorhandling/Either.worksheet.sc
@@ -1,1 +1,19 @@
 import fpinscala.exercises.errorhandling.Either
+
+val left: Either[Int, Int] = Either.Left(0)
+val right: Either[Int, Int] = Either.Right(1)
+
+left.map(_ + 1)
+right.map(_ + 1)
+
+left.flatMap(x => Either.Right(x + 1))
+right.flatMap(x => Either.Right(x + 1))
+
+left.orElse(Either.Right(2))
+right.orElse(Either.Right(2))
+
+left.map2(Either.Right(2))(_ + _)
+right.map2(Either.Right(2))(_ + _)
+
+left.map2(Either.Right(2))(_ * _)
+right.map2(Either.Right(2))(_ * _)
