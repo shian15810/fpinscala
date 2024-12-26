dbb12c02bad42890739851c48dd7052d27c191a7
Cheeshian Chuah <shian15810@gmail.com>
2024-12-26 07:01:47 +0000

    refer to answer for exercise 6.10 with passing tests

--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -81,17 +81,20 @@ object RNG:
 opaque type State[S, +A] = S => (A, S)
 
 object State:
+  def unit[S, A](a: A): State[S, A] = s => (a, s)
+
   extension [S, A](underlying: State[S, A])
     def run(s: S): (A, S) = underlying(s)
 
     def map[B](f: A => B): State[S, B] =
-      ???
+      flatMap(a => unit(f(a)))
 
     def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-      ???
+      flatMap(a => sb.map(b => f(a, b)))
 
-    def flatMap[B](f: A => State[S, B]): State[S, B] =
-      ???
+    def flatMap[B](f: A => State[S, B]): State[S, B] = s =>
+      val (a, s1) = underlying(s)
+      f(a)(s1)
 
   def apply[S, A](f: S => (A, S)): State[S, A] = f
 
